e98853629b4979c3a7fd31d6ff2e744457a5c092
hassanbinilyas <hassanbinilyas@outlook.com>
2026-06-25 23:08:10 +0500

    fix port

--- server.js
+++ server.js
@@ -5,72 +5,60 @@ const usersRouter = require("./routes/api/users");
 const config = require("config");
 const helmet = require("helmet");
 const path = require("path");
+const winston = require("winston");
+const https = require("https");
+const fs = require("fs");
 
 const app = express();
 
-const winston = require('winston');
-
+// Logger
 const logger = winston.createLogger({
-    transports: [
-        new winston.transports.Console(),
-        new winston.transports.File({ filename: 'security.log' })
-    ]
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "security.log" })
+  ]
 });
+logger.info("Application started");
 
-logger.info('Application started');
-
-// Body parser middleware
+// Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// Apply Helmet middleware
 app.use(helmet());
 
 // DB Config
 const db = config.get("mongoURI");
+mongoose.connect(db)
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
 
-// Connect to MongoDB
-mongoose
-    .connect(db)
-    .then(() => console.log("MongoDB successfully connected"))
-    .catch(err => console.log(err));
-
-// Passport middleware
+// Passport
 app.use(passport.initialize());
-
-// Passport config
 require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", usersRouter);
 
-// Serve static assets if in production
+// Production assets
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
-
-const PORT = process.env.PORT || 5000;
-const https = require('https');
-const fs = require('fs');
 
 // SSL options
 const httpsOptions = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
+  key: fs.readFileSync("key.pem"),
+  cert: fs.readFileSync("cert.pem")
 };
 
-// Create HTTPS server
+// HTTPS server only
 const httpsServer = https.createServer(httpsOptions, app);
-
 httpsServer.listen(8443, () => {
   console.log("HTTPS Server Listening on port 8443");
 });
 
-app.get('/', (req, res) => {
-  res.send('HTTPS server is running securely!');
+// Test route
+app.get("/", (req, res) => {
+  res.send("HTTPS server is running securely!");
 });
-
-
